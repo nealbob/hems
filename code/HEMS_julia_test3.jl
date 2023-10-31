aee0a7539063f7b4257e67b5acd26fabcfad9c36
nealbob <hughes.neal@gmail.com>
2023-10-31 15:39:31 +1100

    More updates...

--- code/HEMS_julia_test3.jl
+++ code/HEMS_julia_test3.jl
@@ -14,64 +14,68 @@ n = 3
 β1 = [-0.5, -0.25, -0.25]           # demand function slope
 βc0 = [0.0, 10.0, 0.0]              # future price function constant
 βc1 = [0.0, -5.0, 0.0]              # future price function slope
-tl = [-10e100, -2.0, -1.0]          # trade lower (export) limit 
-tu = [10e100, 0.5, 0.5]             # trade upper (import) limit
-cu = [0.5, 0.5, 0.5]                # carryover limit
+tr_l = [-10e100, -2.0, -1.0]          # trade lower (export) limit 
+tr_u = [10e100, 0.5, 0.5]             # trade upper (import) limit
+c_u = [0.5, 0.5, 0.5]                # carryover limit
 
-function solve_market(n, β0, β1, βc0, βc1, tl, tu, cu, a, c0)
+
+function solve_market(n, β0, β1, βc0, βc1, tr_l, tr_u, c_u, a, c0, t, results)
     
     REGIONS = 1:n
     REG_2n = 2:n                        # first region has no trade limit (i.e., end of the river)
 
-    model = Model(PATHSolver.Optimizer)
-
+    model = Model(PATHSolver.Optimizer, add_bridges=false)
+    set_string_names_on_creation(model, false)
     set_silent(model)
 
     @variables(model, begin
-        tl[r] <= t[r in REGIONS]  <= tu[r], (start = 0)                     # trade 
-        p[r in REGIONS] >= 0, (start = 5)                                   # prices 
-        0 <= c[r in REGIONS] <= cu[r], (start = 0)                          # carryover
+        tr_l[r] <= tr[r in REGIONS]  <= tr_u[r], (start = 0)                     # trade 
+        p[r in REGIONS] >= 0, (start = 1)                                   # prices 
+        0 <= c[r in REGIONS] <= c_u[r], (start = 0)                          # carryover
     end)
 
     q = @expression(model, [r in REGIONS], β0[r] + β1[r] * p[r])                    # use
     E_p = @expression(model, [r in REGIONS], βc0[r] + βc1[r] * c[r])                # expected prices
-    xs = @expression(model, [r in REGIONS], a[r] + c0[r]  + t[r] - c[r] - q[r])     # excess supply
+    xs = @expression(model, [r in REGIONS], a[r] + c0[r]  + tr[r] - c[r] - q[r])     # excess supply
     
     @constraints(model, begin
-        [r in REG_2n],  p[1] - p[r] ⟂ t[r]                                  # equalise prices st trade constraints
+        [r in REG_2n],  p[1] - p[r] ⟂ tr[r]                                  # equalise prices st trade constraints
         [r in REGIONS], xs[r] ⟂ p[r]                                        # demand = supply st price > 0
         [r in REGIONS], p[r] - E_p[r] ⟂ c[r]                                # current price = future price st carryover > 1
-        sum(t) ⟂ t[1]                                                       # sum trade = 0 (use t[1] as complement)
+        sum(tr) ⟂ tr[1]                                                       # sum trade = 0 (use t[1] as complement)
     end)
 
     optimize!(model)
 
-    result = DataFrame("Region"=>REGIONS, 
-                   "Price"=>value.(p).data, 
-                   "Trade"=>value.(t).data,
-                   "Supply"=>a,
-                   "Use"=>value.(q).data,
-                   "Carryover"=>value.(c).data,
-                   "Excess supply"=>value.(xs).data
-                   )
+    #solution_summary(model; verbose=true)
+
+    t0 = t * n - 2
+    tn = t0 + 2
+    results[t0:tn, "Time"] .= t
+    results[t0:tn, "Region"] = REGIONS 
+    results[t0:tn, "Price"] = value.(p).data 
+    results[t0:tn, "Trade"] = value.(tr).data
+    results[t0:tn, "Supply"] = a
+    results[t0:tn, "Use"] = value.(q).data
+    results[t0:tn, "Carryover"] = value.(c).data
+    results[t0:tn, "Excess"] = value.(xs).data
     
-    return result
+    return results
 end
 
-function run_sim(T, n, β0, β1, βc0, βc1, tl, tu, cu, c0)
+function run_sim(T, n, β0, β1, βc0, βc1, tr_l, tr_u, c_u, c0)
     
-    results = DataFrame()
+    cols = ["Time", "Region", "Price", "Trade", "Supply", "Use", "Carryover", "Excess"]
+    results = DataFrame(zeros(T*n, length(cols)), cols)
     
     for t in 1:T
 
         a = rand(3)*5                     # random allocation between 0 and 5
 
-        res_t = solve_market(n, β0, β1, βc0, βc1, tl, tu, cu, a, c0)
+        results = solve_market(n, β0, β1, βc0, βc1, tr_l, tr_u, c_u, a, c0, t, results)
         
-        c0 = res_t.Carryover               # Opening carryover t+1 = closing carryover t
-        res_t[!, :Time] .= t
-        results = vcat(results, res_t)     # Store results for period t
-
+        c0 = res_t[:, "Carryover"]               # Opening carryover t+1 = closing carryover t
+        
     end
 
     return results
@@ -79,6 +83,12 @@ end
 
 c0 = [0, 0, 0]              # opening carryover
 
-@time begin
-    results = run_sim(500, n, β0, β1, βc0, βc1, tl, tu, cu, c0)
-end
+results = run_sim(100, n, β0, β1, βc0, βc1, tr_l, tr_u, cu, c0)
+
+#@profview  results = run_sim(10, n, β0, β1, βc0, βc1, tr_l, tr_u, c_u, c0)
+
+#@profview  results = run_sim(10000, n, β0, β1, βc0, βc1, tr_l, tr_u, c_u, c0)
+
+#@time results = run_sim(50, n, β0, β1, βc0, βc1, tr_l, tr_u, cu, c0)
+
+#@time results = run_sim(10000, n, β0, β1, βc0, βc1, tr_l, tr_u, cu, c0)
---
+++ code/HEMS_julia_test4.jl
@@ -0,0 +1,119 @@
+#=
+    Example of using Julia to solve toy economic equilbirum model
+    n regions, exogenous supply, linear demand and trade subject to constraints
+    PLUS carryover into the next period
+    Set up as an MCP and solve for equilbirum prices and net trade volumes
+    PLUS solve the model many times in a loop / sequence
+=#
+
+using JuMP, PATHSolver, DataFrames, Random
+
+# initial parameters
+n = 3                               # Number of regions
+β = [10.0 5.0 7.5;                  # demand function parameters (X by region)
+    -0.5 -0.25 -0.25]
+βc = [0.0 10.0 0.0;                 # future price function parameters (X by region)
+      0.0 -5.0 0.0]              
+tr_l = [-10e100 -2.0 -1.0;          # trade limits (min-max by region) 
+        10e100 0.5 0.5]             
+c_l = [0.0 0.0 0.0;                 # carryover limits (min-max by regio`n)
+       0.5 0.5 0.5]                
+c0 = [0, 0, 0]                      # opening carryover
+
+function solve_market(n, β, βc, tr_l, c_l, a, t, T, results)
+    
+    REGIONS = 1:n
+    REG_2n = 2:n                        # first region has no trade limit (i.e., end of the river)
+
+    t0 = t * n - 2
+    tn = t0 + 2
+    results[t0:tn, "Time"] .= t
+    c0 = results[results.Time.==t,:Carryover]                               # Opening carryover
+
+    model = Model(PATHSolver.Optimizer, add_bridges=false)
+    set_string_names_on_creation(model, false)
+    set_silent(model)
+
+    @variables(model, begin
+        tr_l[1,r] <= tr[r in REGIONS]  <= tr_l[2,r], (start = 0)                     # trade 
+        p[r in REGIONS] >= 0, (start = 1)                                   # prices 
+        c_l[1,r] <= c[r in REGIONS] <= c_l[2,r], (start = 0)                          # carryover
+    end)
+
+    q = @expression(model, [r in REGIONS], β[1,r] + β[2,r] * p[r])                    # use
+    E_p = @expression(model, [r in REGIONS], βc[1,r] + βc[2,r] * c[r])                # expected prices
+    xs = @expression(model, [r in REGIONS], a[r] + c0[r]  + tr[r] - c[r] - q[r])     # excess supply
+    
+    @constraints(model, begin
+        [r in REG_2n],  p[1] - p[r] ⟂ tr[r]                                  # equalise prices st trade constraints
+        [r in REGIONS], xs[r] ⟂ p[r]                                        # demand = supply st price > 0
+        [r in REGIONS], p[r] - E_p[r] ⟂ c[r]                                # current price = future price st carryover > 1
+        sum(tr) ⟂ tr[1]                                                       # sum trade = 0 (use t[1] as complement)
+    end)
+
+    optimize!(model)
+
+    #solution_summary(model; verbose=true)
+
+    results[t0:tn, "Region"] = REGIONS 
+    results[t0:tn, "Price"] = value.(p).data 
+    results[t0:tn, "Trade"] = value.(tr).data
+    results[t0:tn, "Supply"] = a
+    results[t0:tn, "Use"] = value.(q).data
+    results[t0:tn, "Excess"] = value.(xs).data
+    if t < T
+        results[t0+n:tn+n, "Carryover"] = value.(c).data
+        results[t0+n:tn+n, "Time"] .= t + 1
+    end
+
+    return results
+end
+
+function run_sim(T, n, β, βc, tr_l, c_l)
+    
+    cols = ["Time", "Region", "Price", "Trade", "Supply", "Use", "Carryover", "Excess"]
+    results = DataFrame(zeros(T*n, length(cols)), cols)
+    
+    for t in 1:T
+
+        a = rand(3)*5                     # random allocation between 0 and 5
+
+        results = solve_market(n, β, βc, tr_l, c_l, a, t, T, results)
+        
+    end
+
+    return results
+end
+
+results = run_sim(100, n, β, βc, tr_l, c_l)
+
+@profview  results = run_sim(10, n, β, βc, tr_l, c_l)
+
+@profview  results = run_sim(1000, n, β, βc, tr_l, c_l)
+
+@time results = run_sim(50, n, β, βc, tr_l, c_l)
+
+@time results = run_sim(10000, n, β, βc, tr_l, c_l)
+
+
+
+function sgd_update(βc, results, n, T, i)
+   
+    η = 1 / (1 + i)
+
+    for r in 1:n
+        X = Array{Float64}(undef, T, 2) 
+        X[:,1] .= 1.0
+        X[:,2] .= results[results.Region.==r, :Carryover]
+        Y = results[results.Region.==r, :Price]
+        
+        Ŷ = X * βc[:,r]
+        ϵ = Y - Ŷ
+        ΔQ = transpose(sum((ϵ .* X) ./ T, dims=1))
+        
+        βc[:, r] = βc[:, r] + η * ΔQ 
+    end 
+
+
+    return β    
+end
